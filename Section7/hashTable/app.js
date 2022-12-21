7f648cfa5db5485186333fdd77c02bb017156c29
Lazar Stojanović <lakisd98@gmail.com>
2022-12-21 22:03:46 +0100

    Implemented hash table data structure from scratch

---
+++ Section7/hashTable/app.js
@@ -0,0 +1,64 @@
+class HashTable {
+	constructor(size) {
+		this.data = new Array(size);
+	}
+
+	// Should be private method
+	_hash(key) {
+		let hash = 0;
+		for (let i = 0; i < key.length; i++) {
+			hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+		}
+		return hash;
+	}
+
+	set(key, value) {
+		let hash = this._hash(key);
+		if (!this.data[hash]) {
+			this.data[hash] = [[key, value]];
+		} else {
+			for (let pair of this.data[hash]) {
+				if (key == pair[0]) {
+					pair[1] = value;
+					return;
+				}
+			}
+			this.data[hash].push([key, value]);
+		}
+	}
+
+	get(key) {
+		let hash = this._hash(key);
+		const currentBucket = this.data[hash];
+		if (currentBucket) {
+			for (let i = 0; i < currentBucket.length; i++) {
+				if (currentBucket[i][0] === key) {
+					return currentBucket[i][1];
+				}
+			}
+		}
+		return undefined;
+	}
+
+	keys() {
+		if (!this.data.length) {
+			return undefined;
+		}
+		let result = [];
+		// loop through all the elements
+		for (let i = 0; i < this.data.length; i++) {
+			// if it's not an empty memory cell
+			if (this.data[i] && this.data[i].length) {
+				// but also loop through all the potential collisions
+				if (this.data.length > 1) {
+					for (let j = 0; j < this.data[i].length; j++) {
+						result.push(this.data[i][j][0]);
+					}
+				} else {
+					result.push(this.data[i][0]);
+				}
+			}
+		}
+		return result;
+	}
+}
