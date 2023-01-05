6aa6ea420e99da0754a421a40e59ec5ad33ba928
Lazar Stojanović <lakisd98@gmail.com>
2023-01-05 14:24:07 +0100

    Implemented graph data structure from scratch

---
+++ graphs/graphTypes/app.js
@@ -0,0 +1,18 @@
+// Edge List (Relation data graph)
+const edgeGraph = [
+	[0, 2],
+	[2, 3],
+	[2, 1],
+	[1, 3],
+];
+
+// Adjacent List (Adjacent elements graph by index representation)
+const adjacentGraph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent matrix (1 indicates which index is connected to specific node)
+const matrixGraph = [
+	[0, 0, 1, 0],
+	[0, 0, 1, 1],
+	[1, 1, 0, 1],
+	[0, 1, 1, 0],
+];
